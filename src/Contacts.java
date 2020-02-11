8596b22d435ba4111ced087e2274d9c507f68a13
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-11 13:37:11 -0600

    in progress

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,6 +1,5 @@
 package src;
 
-
 public class Contacts {
 
     private String firstName;   // firstName is a data member.
@@ -13,47 +12,47 @@ public class Contacts {
         this.phoneNumber = phoneNumber;
     }
 
-    /**
-     * Sets the value of firstName to "newFirstName".
-     **/
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Sets the value of lastName to "newLastName".
-     **/
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Sets the value of phoneNumber to "newPhoneNumber".
-     **/
-    public void setphoneNumber(String PhoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * Return the value of firstName.
-     **/
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Returns the value of lastName.
-     **/
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Returns the value of phoneNumber.
-     **/
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+//    /**
+//     * Sets the value of firstName to "newFirstName".
+//     **/
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    /**
+//     * Sets the value of lastName to "newLastName".
+//     **/
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    /**
+//     * Sets the value of phoneNumber to "newPhoneNumber".
+//     **/
+//    public void setphoneNumber(String PhoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    /**
+//     * Return the value of firstName.
+//     **/
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    /**
+//     * Returns the value of lastName.
+//     **/
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    /**
+//     * Returns the value of phoneNumber.
+//     **/
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
 
     public String toString() {
         return this.firstName + " " + this.lastName + "Phone number: " + this.phoneNumber;
